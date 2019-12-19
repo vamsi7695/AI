849f289dff5ca44b10a9b852cf566458d0ecf3fa
Vamsi Sai Babu Pasumarthi <vpasumarthi@worldbankgroup.org>
2019-12-19 19:20:51 +0530

    first

---
+++ precache-manifest.14a43c8a9dadd6364906f3947fb9da15.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2200d2dac3fcd1ef0838f547eb337595",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e3bbea1223b4f16ad18",
+    "url": "/static/css/main.6d6624bf.chunk.css"
+  },
+  {
+    "revision": "8b62598a195c1c257beb",
+    "url": "/static/js/2.75dcedf9.chunk.js"
+  },
+  {
+    "revision": "9e3bbea1223b4f16ad18",
+    "url": "/static/js/main.eb41be73.chunk.js"
+  },
+  {
+    "revision": "7bf4234e7642f704a1f2",
+    "url": "/static/js/runtime~main.1c3c4c14.js"
+  },
+  {
+    "revision": "3d596669f67119cdfd4416fdfebdedfc",
+    "url": "/static/media/737789-637027678539043347-16x9.3d596669.jpg"
+  },
+  {
+    "revision": "f2cf2b0f0bf0ab04ac9823884ce68275",
+    "url": "/static/media/ai.f2cf2b0f.png"
+  },
+  {
+    "revision": "a81a657ce8c55d6f2cedc8018718cbc6",
+    "url": "/static/media/articles.a81a657c.png"
+  },
+  {
+    "revision": "a242ae308b2e53ce4beba4cfcac74884",
+    "url": "/static/media/bank.a242ae30.png"
+  },
+  {
+    "revision": "786758c2afbdf80443e613ae9b173b5e",
+    "url": "/static/media/case_studies.786758c2.png"
+  },
+  {
+    "revision": "039e0b0f0c3865c5cf94ce469ba83808",
+    "url": "/static/media/cognitive.039e0b0f.png"
+  },
+  {
+    "revision": "71111070c1b0aea096036c9db3442536",
+    "url": "/static/media/futuristic_workplace.71111070.png"
+  },
+  {
+    "revision": "3dc2d1d7df88c03a28c6e42e963c7e7f",
+    "url": "/static/media/health.3dc2d1d7.png"
+  },
+  {
+    "revision": "b9533491786cb66ee1981bbe20943d38",
+    "url": "/static/media/img_onl.b9533491.png"
+  },
+  {
+    "revision": "b4f8374709a51f0e77ed568691e76205",
+    "url": "/static/media/industry.b4f83747.png"
+  },
+  {
+    "revision": "f1f25370d191a4d5ce359db9ec304adc",
+    "url": "/static/media/manlogretail.f1f25370.png"
+  },
+  {
+    "revision": "62475dd35853ebbf2b06b736d10bc099",
+    "url": "/static/media/natural.62475dd3.png"
+  },
+  {
+    "revision": "fa8364e124f39918ee56f06b4fb23c63",
+    "url": "/static/media/power-platform-slides-676.fa8364e1.png"
+  },
+  {
+    "revision": "82d9b86b01560ce17d310f3fd2a79ca9",
+    "url": "/static/media/samvideo.82d9b86b.mp4"
+  },
+  {
+    "revision": "a450a4d1a7ec07abc8bb92561ac024cc",
+    "url": "/static/media/sentiment-analysisas.a450a4d1.png"
+  },
+  {
+    "revision": "34e19f4643a01f6ff8284a5708d2f749",
+    "url": "/static/media/sourcecode_91536-630x330.34e19f46.png"
+  },
+  {
+    "revision": "bcb1914b5ca5ccd1e74ace777e3c3c8b",
+    "url": "/static/media/techConsul.bcb1914b.png"
+  },
+  {
+    "revision": "5d5cbdfe49cb03020af789b72bdb9c29",
+    "url": "/static/media/techTalks.5d5cbdfe.png"
+  },
+  {
+    "revision": "07372338c886c6fbe2b96a65768353b1",
+    "url": "/static/media/virtual.07372338.png"
+  },
+  {
+    "revision": "f3a8a92c2e32edf190feab5675d17799",
+    "url": "/static/media/white-papers.f3a8a92c.png"
+  }
+]);
